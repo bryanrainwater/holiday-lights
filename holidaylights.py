ca5a2838fe9d6c919d64492f3624030adf81d4ac
Bryan Rainwater <rainwater92@gmail.com>
2023-10-02 02:35:26 -0600

    Ironing out some bugs and working out pos/dir notation for each LED

--- holidaylights.py
+++ holidaylights.py
@@ -12,6 +12,32 @@ import random
 import numpy as np
 import sys
 
+
+sunsetCheck = True
+    
+PIN = board.D18
+NUM_PIXELS=400#300#200
+ORDER = neopixel.RGB
+    
+max_bright = 255
+    
+color1 = (0, 0, 0)
+color2 = (max_bright, max_bright, max_bright)
+red = (max_bright, 0, 0)
+green = (0, max_bright, 0)
+blue = (0, 0, max_bright)
+    
+orange = (255, 20, 0)
+yellow = (255, 90, 0)
+purple = (255, 0, 100)
+white = (255,255,255)#, 0, 100)
+    
+rando = (255,255,0)
+#orange = (255,255,255)
+#purple = (255,255,255)
+
+
+
 # Personal API key python file config.py
 #   of form api_key = "bleh"
 import config
@@ -110,7 +136,7 @@ def get_weather(loc):
         #   not even sure I want this feature
         wx['c'] = cloudCover/10 
 
-    return wx
+    return wx, desc
 
 def get_event(date = None):
     # Events and effects:
@@ -144,27 +170,11 @@ if __name__ == "__main__":
     now = get_timenow(loc)
     sunset = get_sunset(loc)
     
-    PIN = board.D18
-    NUM_PIXELS=200
-    ORDER = neopixel.RGB
-    
-    max_bright = 255
-    
-    color1 = (0, 0, 0)
-    color2 = (max_bright, max_bright, max_bright)
-    red = (max_bright, 0, 0)
-    green = (0, max_bright, 0)
-    blue = (0, 0, max_bright)
-    
-    orange = (255, 20, 0)
-    yellow = (255, 90, 0)
-    purple = (255, 0, 100)
-    
     pixels = neopixel.NeoPixel( PIN, NUM_PIXELS, auto_write = False, pixel_order = ORDER)
     
-    iterator = 0
-    num_rainbows = 4#3
-    skip = 100
+    #iterator = 0
+    num_rainbows = 8# 4#3
+    skip = 0#100
     pattern = None
     
     for i in range(skip):
@@ -174,7 +184,8 @@ if __name__ == "__main__":
     timeCheckInterval = 3
     
     oldWxCheck = time.time()
-    wx = get_weather(loc)
+    wx, wxdesc = get_weather(loc)
+    print("Weather is ", wxdesc)
     wxCheckInterval = 86400/1000 # Get 1000 requests per day for free
     
     nextFlash = time.time()
@@ -182,6 +193,9 @@ if __name__ == "__main__":
     #patternType = "static"#"rolling"
     patternType = "rolling"
     uniqueColors = [orange,orange,purple]#,blue]
+    #uniqueColors = [white,yellow]#,blue]
+    #uniqueColors = [blue, red, green]#white]#, green]#,blue]
+    #uniqueColors = [rando,rando]#,blue]
     numUniqueColors = len(uniqueColors)
     updateDelay = 1#0.2
     
@@ -196,8 +210,9 @@ if __name__ == "__main__":
     
         if time.time() > oldWxCheck + wxCheckInterval:
             print("Updating weather data")
-            wx = get_weather(loc)
+            wx, wxdesc = get_weather(loc)
             oldWxCheck = time.time()
+            print("Weather is ", wxdesc)
     
        
         if( sunset.date() < get_timenow(loc).date() or pattern is None):
@@ -210,7 +225,126 @@ if __name__ == "__main__":
     
             # Set pattern
             pattern = [(0,0,0)]*NUM_PIXELS
-    
+
+            #if event == 'october':
+            #if event == 'halloween':
+            #if event == 'november':
+            #if event == 'december':
+            #if event == 'christmas':
+            #if event == 
+
+
+            # need to come up with scheme for house orientation.
+            #   For example, my house has LEDs of the form:
+            #
+            #                                   ->
+            #                                 ->  ->
+            #                               ->      ->
+            #                             ->          ->
+            #                           ->      <-      ->
+            #                ->->-> V ->      <-  <-    # ->
+            #             ->          |     <-      <- ##  |
+            #           ->            |     |        |     |    
+            #         ->              |     |        |     |
+            #       ->             ____________________________     
+            #     ->               | ======================== |
+            #   ->                 | ======================== |
+            # -> ######### <-<-<-<-^ ======================== |
+            # |            | _____ ^ ======================== |
+            # |            | |   | <-<-<-<-<-<-<-<-<-<-<-<-<-<-
+            # |            | |   | |                          |                         
+            # |            | |   | |                          |
+            # |            | _____ |                          |
+            # |            |_______|                          |
+            # |                    |                          | 
+            # |                    |                          |
+            #
+            #
+            #   Note the direction of arrows, left right is obvious,
+            #   down arrow is towards the front, carrot is into house
+            #   hash tags are led strips connected by general wiring
+            #   equal signs are roofing shingles over garage leading 
+            #   up to next level
+
+            #   Premise of LEDs, each LED needs to know it's directionality
+            #   and position so that everything can be cycled correctly.
+
+            #   So let's break LED's into segments as a list in cartesian space
+            #   starting at the first LED in the strip (seeing as data is daisy chained)
+
+            #   In my case, the lowest right pixel is the starting pixel.
+            #       So 30 ft, 90 pixels, moving to the left
+            #       Then 5 ft, 15 pixels towards the house
+            #       Then 8 ft, 24 pixels over top of front door, moving left
+            #           dark wire running to the furthest left pixel
+            #       Then 21 ft, 63 pixels moving from left to right
+            #       Then 10 ft, 30 pixels moving away from house
+            #       Then 30 ft, 60 pixels moving up and over topmost peak towards right
+            #           dark wire running from topmost peak on right to recessed peak
+            #       Then 16 ft, 48 pixels moving from righ to left
+            #           End of sequence
+
+            #       45 degree steep peaks move sqrt(2) slower than horizontal LEDs
+            #       Program in speed adustment
+
+            #       Need to map everything to "pixel" cartesian space
+
+            #       In other words, leds across front of garage are 30 ft,
+            #           but this is -90 pixels towards the left, and the topmost 
+            #           peak of house does not fully extend to the 90th pixel 
+            #           nor the 0th pixel of the garage line
+
+            #       Therefore, we could say, the topmost peak runs from pixel
+            #           -70 to -45, at +45 degrees, and -45 to -20 at -45 deg
+            
+            #       Similarly, we could say into or out of the house are at
+            #           -90 or +90 degrees.
+
+            #       So to build this, we could do two arrays at total length of pixels,
+            #           one to denote angle
+            
+
+            # So first strip is 90 pixels, 180 deg, starts at pos 0
+            #   second strip is 15 pixels, 90 deg, starts at pos -90
+            #   third strip is  30 pixels, 180 deg, starts at pos -90
+            #   fourth strip is 90 pixels, 45 deg, ends at pos -90
+            #   fifth strip is 30 pixels, -90 deg, ends at pos -90
+            #   sixth strip is 50 pixels, 45 deg, starts at pos -70
+            #   seventh strip is 50 pixels, -45 deg, starts at pos -45
+            #   eigth strip is 25 pixels, 135 deg, starts at pos -30
+            #   ningth strip is 25 pixels, -135 deg, starts at pos -45
+
+            # break this into strips.....
+            #   Each row is a new strip, with columns
+            #       [#LEDs, strt, angle, end ]
+            #   Noting that start and end are in LED counts
+
+            strips = [ 
+                    [ 90,    0,  180,   -90 ],
+                    [ 15,  -90,   90,  None ],
+                    [ 30,  -90,  180,  None ],
+                    [ 90, None,   45,   -90 ],
+                    ]
+
+            # Once I have all my strips loaded, need to fill in None values
+            #   by calculating the end start or end positions based on angles
+
+            for strip in strips:
+                if strip[1] == None:
+                    # This means start position needs to be calculated
+                    strip[1] = np.cos(strip[2]) * strip[0]
+                if strip[3] == None:
+                    strip[3] = np.cos(strip[1]) * strip[0]
+
+            #   Next, figure out arrange according to patterns
+
+            #   For example, rainbow I will want an option to have everything going
+            #       smoothly from left to right.
+
+            #   To do this, I will need to put everything into a horizontal matrix
+            #       
+
+
             event = 'october'
             #event = 'pride'#'october'
             if event == 'october':
@@ -220,7 +354,7 @@ if __name__ == "__main__":
                     updateDelay = 1
             if event == "pride":
                 for i in range(skip,NUM_PIXELS):
-                    val = (i+iterator)/(NUM_PIXELS/num_rainbows)# + iterator/(NUM_PIXELS)
+                    val = i/(NUM_PIXELS/num_rainbows)
                     while(val > 1.0):
                         val -= 1.0
                     (r, g, b) = colorsys.hsv_to_rgb( val , 1.0, 1.0)
@@ -229,14 +363,8 @@ if __name__ == "__main__":
                     if b>=1.0: b = 1
                     pixels[i] = int(255*r), int(255*g), int(255*b)
                 updateDelay = 0.05
-        #if event == 'october':
-        #if event == 'halloween':
-        #if event == 'november':
-        #if event == 'december':
-        #if event == 'christmas':
-        #if event == 
         
-        #whiteMask = [(0,0,0)]*NUM_PIXELS
+        print("Pattern set to ",event)
     
         wx['t'][0] = 0# 1
         wx['t'][1] = 0# 1
@@ -269,41 +397,31 @@ if __name__ == "__main__":
             
             nextFlash = time.time() + flashDelay
     
-        # Static colors:
-        if(patternType == 'flip'):
-            for j in range(100):
-                if i%2 == 0:
-                    #pixels[2*j] = (255, 0, 0)
-                    pixels[2*j] = (0, 0, 0)
-                    pixels[2*j+1] = (max_bright,max_bright,max_bright)
-                else:
-                    pixels[2*j+1] = (0, 0, 0)
-                    #pixels[2*j+1] = (255, 0, 0)
-                    pixels[2*j] = (max_bright, max_bright, max_bright)
-        elif(patternType == 'rolling'):
-            # This will simply roll by one value everytime
-            currentPixels = pixels[:]
-            currentPixels = currentPixels[skip:]
+        # This will simply roll by one value everytime
+        currentPixels = pixels[:]
+        currentPixels = currentPixels[skip:]
     
-            currentPixels = np.roll(currentPixels,3)
-            for i in range(skip,NUM_PIXELS):
-                pixels[i] = currentPixels[i-skip]
+        currentPixels = np.roll(currentPixels,3)
+        for i in range(skip,NUM_PIXELS):
+            pixels[i] = currentPixels[i-skip]
         
         # Final check to see if before sunset
         #   if before sunset, calculate wait until sunset and wait
         #   if after sunset, wait until date rolls over.
-        if( sunset > get_timenow(loc) ) :
-            print("Beginning the long wait. Number of seconds until sunset: ", (sunset - get_timenow(loc)).seconds)
+        if( sunsetCheck and sunset > get_timenow(loc) ) :
+            print("Beginning wait. Number of seconds until sunset: ", (sunset - get_timenow(loc)).seconds)
             # Turning off pixels
             for i in range(NUM_PIXELS):
                 pixels[i] = (0,0,0)
             pixels.show()
-            # Now beginning the long wait
-            updateDelay = (sunset - get_timenow(loc)).seconds
-    
+            # Waiting a few minutes, check again
+            #   I could wait until sunset, butttt, nice to
+            #   see code is still churning in systemctl
+            #updateDelay = (sunset - get_timenow(loc)).seconds
+            updateDelay = 900
+
         pixels.show()
         sys.stdout.flush()
         time.sleep(updateDelay)#0.05)
-        iterator+=1
-        if iterator > NUM_PIXELS: iterator = 0
-
+        #iterator+=1
+        #if iterator > NUM_PIXELS: iterator = 0
